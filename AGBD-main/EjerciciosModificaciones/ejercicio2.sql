9319c1d20b9603c5e92cf9d95c23d4743c3a7542
sofia flores <bf917242@gmail.com>
2024-05-30 10:18:55 -0300

    Ejercicio con insert

---
+++ AGBD-main/EjerciciosModificaciones/ejercicio2.sql
@@ -0,0 +1,2 @@
+INSERT INTO albums (title,ArtistId)
+VALUES ("El templo del pop", 276)
---
+++ AGBD-main/EjerciciosModificaciones/ejercicio3.sql
@@ -0,0 +1,2 @@
+INSERT INTO tracks (name,  MediaTypeId, Milliseconds, UnitPrice)
+VALUES ("Bailarina", "Iman", "Tu Juego")
